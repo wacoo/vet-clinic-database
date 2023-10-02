b7147be0e5ab11ed01a7c66fda75716aa67b7d7f
wac <wabaham9@gmail.com>
2023-10-02 15:40:35 +0300

    Add bulk insert

--- queries.sql
+++ queries.sql
@@ -82,6 +82,6 @@ SELECT s.name AS speciality_name FROM (SELECT a.species_id, COUNT(*) AS visit_co
 
 -- proje 5
 
-SELECT COUNT(*) FROM visits where animal_id = 4;
-SELECT * FROM visits where vet_id = 2;
-SELECT * FROM owners where email = 'owner_18327@mail.com';
+EXPLAIN ANALYZE SELECT COUNT(*) FROM visits where animal_id = 4;
+EXPLAIN ANALYZE SELECT * FROM visits where vet_id = 2;
+EXPLAIN ANALYZE SELECT * FROM owners where email = 'owner_18327@mail.com';
